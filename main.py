1638bd38220a730573b43e1be41b604a02f46020
Daniel Evans <me@daredoes.work>
2019-04-29 21:07:19 -0700

    remove builtin UAC prompt

--- main.py
+++ main.py
@@ -360,63 +360,6 @@ def message_received(client, server, message):
     except (json.JSONDecodeError, KeyError) as e:
         server.send_message(client, make_message_for_client('Cannot read message: {}'.format(e), 'info'))
 
-import ctypes
-import enum
-import subprocess
-import sys
-
-# Reference:
-# msdn.microsoft.com/en-us/library/windows/desktop/bb762153(v=vs.85).aspx
-
-
-# noinspection SpellCheckingInspection
-class SW(enum.IntEnum):
-    HIDE = 0
-    MAXIMIZE = 3
-    MINIMIZE = 6
-    RESTORE = 9
-    SHOW = 5
-    SHOWDEFAULT = 10
-    SHOWMAXIMIZED = 3
-    SHOWMINIMIZED = 2
-    SHOWMINNOACTIVE = 7
-    SHOWNA = 8
-    SHOWNOACTIVATE = 4
-    SHOWNORMAL = 1
-
-
-class ERROR(enum.IntEnum):
-    ZERO = 0
-    FILE_NOT_FOUND = 2
-    PATH_NOT_FOUND = 3
-    BAD_FORMAT = 11
-    ACCESS_DENIED = 5
-    ASSOC_INCOMPLETE = 27
-    DDE_BUSY = 30
-    DDE_FAIL = 29
-    DDE_TIMEOUT = 28
-    DLL_NOT_FOUND = 32
-    NO_ASSOC = 31
-    OOM = 8
-    SHARE = 26
-
-
-def bootstrap():
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        main()
-    else:
-       # noinspection SpellCheckingInspection
-        hinstance = ctypes.windll.shell32.ShellExecuteW(
-            None,
-            'runas',
-            '"{}"'.format(sys.executable),
-            subprocess.list2cmdline(sys.argv),
-            None,
-            SW.SHOWNORMAL
-        )
-        if hinstance <= 32:
-            raise RuntimeError(ERROR(hinstance))
-
 
 def main():
     while not user.is_in_correct_folder:
