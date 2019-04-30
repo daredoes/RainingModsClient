c8a552c49cf3827c5f00f02050781f8f1e1f96c9
Daniel Evans <me@daredoes.work>
2019-04-29 21:06:39 -0700

    add human-readable support and installing/updating mods

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from websocket_server import WebsocketServer
 import tkinter as tk
 from tkinter import filedialog
 
-import ctypes, sys
-
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
@@ -33,26 +31,30 @@ files_in_risk_of_rain_2_root_folder_with_bepin = ["winhttp.dll", "BepInEx"]
 def convert_mod_data_to_object(data, current_user):
     mods = []
     for author_id in data.keys():
-        mod = ModAuthor(author_id, current_user)
-        for repo_id in data[author_id]:
-            repo = ModRepo(repo_id)
-            for release_id in data[author_id][repo_id]:
-                release = ModRelease(release_id)
-                for asset_id in data[author_id][repo_id][release_id]:
-                    asset_data = data[author_id][repo_id][release_id][asset_id]
-                    asset = ModReleaseAsset(asset_id, asset_data['download'], asset_data['path'])
-                    release.add_asset(asset)
-                repo.add_release(release)
+        author_data = data[author_id]
+        mod = ModAuthor(author_id, current_user, author_data.get('name'), author_data.get('url'))
+        for repo_id in author_data.get('repos', {}):
+            repo_data = author_data['repos'][repo_id]
+            repo = ModRepo(repo_id, repo_data.get('name'), repo_data.get('url'), repo_data.get('readme'), repo_data.get('description'))
+            release_data = repo_data.get('release', {})
+            release = ModRelease(release_data.get('id'), release_data.get('name'), release_data.get('tag_name'), release_data.get('url'))
+            for asset_id in release_data.get('assets', {}):
+                asset_data = release_data['assets'][asset_id]
+                asset = ModReleaseAsset(asset_id, asset_data.get('download'), asset_data.get('path'), asset_data.get('name'), asset_data.get('content_type'))
+                release.add_asset(asset)
+            repo.add_release(release)
             mod.add_repo(repo)
         mods.append(mod)
     return mods
 
 
 class ModAuthor(object):
-    def __init__(self, id, user):
-        self.id = id
+    def __init__(self, git_id, user, name, url):
+        self.id = git_id
         self.user = user
         self.repos = []
+        self.name = name
+        self.url = url
 
     def add_repo(self, repo):
         if isinstance(repo, ModRepo):
@@ -65,7 +67,12 @@ class ModAuthor(object):
         for repo in self.repos:
             repos.update(repo.to_dict())
         return {
-            self.id: repos
+            self.id: {
+                'name': self.name,
+                'url': self.url,
+                'id': self.id,
+                'repos': repos,
+            }
         }
 
     def install(self):
@@ -74,7 +81,7 @@ class ModAuthor(object):
             child.install()
         logger.info('Finalizing install of {}'.format(self.id))
         plugin_root = self.user.plugin_path
-        plugin_path = os.path.join(plugin_root, self.id)
+        plugin_path = os.path.join(plugin_root, self.name)
         os.makedirs(plugin_path, exist_ok=True)
         json.dump(self.to_dict(), open(os.path.join(plugin_path, 'RainingMods.json'), 'w'), sort_keys=True, indent=4, separators=(',', ': '))
         logger.info('Ended install of {}'.format(self.id))
@@ -93,23 +100,31 @@ class ModAuthor(object):
                 return False
     
 class ModRepo(object):
-    def __init__(self, id):
-        self.id = id
-        self.releases = []
+    def __init__(self, git_id, name, url, readme, description):
+        self.id = git_id
+        self.release = None
+        self.name = name
+        self.url = url
+        self.readme = readme
+        self.description = description
         self.author = None
 
     def add_release(self, release):
         if isinstance(release, ModRelease):
-            if release not in self.releases:
+            if release != self:
                 release.repo = self
-                self.releases.append(release)
+                self.release = release
     
     def to_dict(self):
-        releases = {}
-        for release in self.releases:
-            releases.update(release.to_dict())
         return {
-            self.id: releases
+            self.id: {
+                'release': self.release.to_dict() if self.release else {},
+                'name': self.name,
+                'url': self.url,
+                'readme': self.readme,
+                'description': self.description,
+                'id': self.id,
+            }
         }
 
     def install(self):
@@ -118,7 +133,7 @@ class ModRepo(object):
 
     @property
     def children(self):
-        return self.releases
+        return [self.release]
 
     def __cmp__(self, other):
         if isinstance(other, self.__class__):        
@@ -130,9 +145,12 @@ class ModRepo(object):
                 return False
 
 class ModRelease(object):
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, git_id, name, tag_name, url):
+        self.id = git_id
         self.assets = []
+        self.name = name
+        self.tag_name = tag_name
+        self.url = url
         self.repo = None
 
     def add_asset(self, asset):
@@ -146,7 +164,11 @@ class ModRelease(object):
         for asset in self.assets:
             assets.update(asset.to_dict())
         return {
-            self.id: assets
+            'assets': assets,
+            'id': self.id,
+            'name': self.name,
+            'tag_name': self.name,
+            'url': self.url
         }
 
     def install(self):
@@ -168,27 +190,33 @@ class ModRelease(object):
 
 class ModReleaseAsset(object):
     
-    def __init__(self, id, download_url, path=None):
-        self.id = id
+    def __init__(self, git_id, download_url, name, content_type, path=None):
+        self.id = git_id
         self.download_url = download_url
         self._path = path
+        self.name = name
+        self._content_type = content_type
         self.release = None
 
     def to_dict(self):
         return {
             self.id: {
                 'path': self._path,
-                'download': self.download_url
+                'download': self.download_url,
+                'name': self.name,
+                'id': self.id,
             }
         }
 
     def install(self):
         if self.release and self.release.repo and self.release.repo.author and self.release.repo.author.user and self.release.repo.author.user.has_bepin:
             plugin_root = self.release.repo.author.user.plugin_path
-            plugin_path = os.path.join(plugin_root, self.release.repo.author.id, self.release.repo.id, self.release.id)
-            shutil.rmtree(plugin_path, ignore_errors=True)
+            plugin_path = os.path.join(plugin_root, self.release.repo.author.name, self.release.repo.name)
+            for path in glob.glob('{}/*'.format(plugin_path)):
+                shutil.rmtree(path, ignore_errors=True)
+            plugin_path = os.path.join(plugin_path, self.release.name)
             os.makedirs(plugin_path, exist_ok=True)
-            wget.download(self.download_url, plugin_path)
+            self._path = wget.download(self.download_url, plugin_path)
         
     def __cmp__(self, other):
         try:
@@ -307,19 +335,8 @@ def get_user_data(server, client, **args):
     server.send_message(client, make_message_for_client('update', 'update', {'user': str(user)}))
 
 def install_mod_version(server, client, **args):
-    for author_id in args.keys():
-        mod_author = ModAuthor(author_id, user)
-        for repo_id in args[author_id]:
-            mod_repo = ModRepo(repo_id)
-            mod_author.add_repo(mod_repo)
-            for release_id in args[author_id][repo_id]:
-                mod_release = ModRelease(release_id)
-                mod_repo.add_release(mod_release)
-                for asset_id in args[author_id][repo_id][release_id]:
-                    download_url = args[author_id][repo_id][release_id][asset_id]['download']
-                    mod_asset = ModReleaseAsset(asset_id, download_url)
-                    mod_release.add_asset(mod_asset)
-        user.add_mod(mod_author, install=True)
+    for mod in convert_mod_data_to_object(args, user):
+        user.add_mod(mod, install=True)
     get_user_data(server, client)
             
 
@@ -343,21 +360,81 @@ def message_received(client, server, message):
     except (json.JSONDecodeError, KeyError) as e:
         server.send_message(client, make_message_for_client('Cannot read message: {}'.format(e), 'info'))
 
+import ctypes
+import enum
+import subprocess
+import sys
+
+# Reference:
+# msdn.microsoft.com/en-us/library/windows/desktop/bb762153(v=vs.85).aspx
 
 
-if __name__ == "__main__":
-    if is_admin():
-        # Code of your program here
-        while not user.is_in_correct_folder:
-            if not pick_root_folder():
-                logger.info('Sleeping for 2 seconds. Quit now, or select the folder where Risk of Rain 2.exe is located.')
-                time.sleep(2)
-        server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
-        server.set_fn_new_client(new_client)
-        server.set_fn_message_received(message_received)
-        server.run_forever()
+# noinspection SpellCheckingInspection
+class SW(enum.IntEnum):
+    HIDE = 0
+    MAXIMIZE = 3
+    MINIMIZE = 6
+    RESTORE = 9
+    SHOW = 5
+    SHOWDEFAULT = 10
+    SHOWMAXIMIZED = 3
+    SHOWMINIMIZED = 2
+    SHOWMINNOACTIVE = 7
+    SHOWNA = 8
+    SHOWNOACTIVATE = 4
+    SHOWNORMAL = 1
+
+
+class ERROR(enum.IntEnum):
+    ZERO = 0
+    FILE_NOT_FOUND = 2
+    PATH_NOT_FOUND = 3
+    BAD_FORMAT = 11
+    ACCESS_DENIED = 5
+    ASSOC_INCOMPLETE = 27
+    DDE_BUSY = 30
+    DDE_FAIL = 29
+    DDE_TIMEOUT = 28
+    DLL_NOT_FOUND = 32
+    NO_ASSOC = 31
+    OOM = 8
+    SHARE = 26
+
+
+def bootstrap():
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        main()
     else:
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+       # noinspection SpellCheckingInspection
+        hinstance = ctypes.windll.shell32.ShellExecuteW(
+            None,
+            'runas',
+            '"{}"'.format(sys.executable),
+            subprocess.list2cmdline(sys.argv),
+            None,
+            SW.SHOWNORMAL
+        )
+        if hinstance <= 32:
+            raise RuntimeError(ERROR(hinstance))
 
 
+def main():
+    while not user.is_in_correct_folder:
+        if not pick_root_folder():
+            logger.info('Sleeping for 2 seconds. Quit now, or select the folder where Risk of Rain 2.exe is located.')
+            time.sleep(2)
+    server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
+    server.set_fn_new_client(new_client)
+    server.set_fn_message_received(message_received)
+    server.run_forever()
+
+
+if __name__ == '__main__':
+    while not user.is_in_correct_folder:
+        if not pick_root_folder():
+            logger.info('Sleeping for 2 seconds. Quit now, or select the folder where Risk of Rain 2.exe is located.')
+            time.sleep(2)
+    server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
+    server.set_fn_new_client(new_client)
+    server.set_fn_message_received(message_received)
+    server.run_forever()
